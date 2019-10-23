8a0e8a3c3dd46d23a20fa5af43eb6e1e1aa26996
saumyadebbiswas <>
2019-10-23 12:39:52 +0530

    test boostrap sidebar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -146,4 +146,13 @@ export class HomePage {
     }
   }
 
+  openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
 }
